3911d7d3421e73f7a6529b6bc268902f410a180f
Roberto <roberto.guarneros11@gmail.com>
2023-10-10 12:43:20 +0200

    Missing equal function

--- app/src/main/java/com/eetac/dsa/calculator/MainActivity.java
+++ app/src/main/java/com/eetac/dsa/calculator/MainActivity.java
@@ -1,7 +1,6 @@
 package com.eetac.dsa.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -19,76 +18,113 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewOperation = (TextView) findViewById(R.id.textViewOperation);
-        textViewResult = (TextView) findViewById(R.id.textViewResult);
+        textViewOperation = findViewById(R.id.textViewOperation);
+        textViewResult = findViewById(R.id.textViewResult);
+    }
 
-    }
-    public void equal(View view) {
-        textViewResult.setText("");
-        textViewResult.append(inputExpression);
-    }
     public void dot(View view) {
         inputExpression.append(".");
         textViewOperation.append(".");
     }
+
     public void zero(View view) {
         inputExpression.append("0");
         textViewOperation.append("0");
     }
+
     public void one(View view) {
         inputExpression.append("1");
         textViewOperation.append("1");
     }
+
     public void two(View view) {
         inputExpression.append("2");
         textViewOperation.append("2");
     }
+
     public void three(View view) {
         inputExpression.append("3");
-        textViewOperation.append("");
+        textViewOperation.append("3");
     }
+
     public void four(View view) {
         inputExpression.append("4");
         textViewOperation.append("4");
     }
+
     public void five(View view) {
         inputExpression.append("5");
         textViewOperation.append("5");
     }
+
     public void six(View view) {
         inputExpression.append("6");
         textViewOperation.append("6");
     }
+
     public void seven(View view) {
         inputExpression.append("7");
         textViewOperation.append("7");
     }
+
     public void eight(View view) {
         inputExpression.append("8");
         textViewOperation.append("8");
     }
+
     public void nine(View view) {
         inputExpression.append("9");
         textViewOperation.append("9");
     }
+
     public void clear(View view) {
         textViewOperation.setText("");
+        textViewResult.setText("");
         inputExpression.setLength(0);
     }
+
     public void sin(View view) {
+        inputExpression.append("sin");
+        textViewOperation.append("sin");
     }
+
     public void tan(View view) {
+        inputExpression.append("tan");
+        textViewOperation.append("tan");
     }
+
     public void cos(View view) {
+        inputExpression.append("cos");
+        textViewOperation.append("cos");
     }
+
     public void divide(View view) {
+        inputExpression.append("/");
+        textViewOperation.append("/");
     }
+
     public void multiply(View view) {
+        inputExpression.append("*");
+        textViewOperation.append("*");
     }
+
     public void subtract(View view) {
+        inputExpression.append("-");
+        textViewOperation.append("-");
     }
+
     public void add(View view) {
+        inputExpression.append("+");
+        textViewOperation.append("+");
     }
+
     public void delete(View view) {
+        if (inputExpression.length() > 0) {
+            inputExpression.deleteCharAt(inputExpression.length() - 1);
+            textViewOperation.setText(inputExpression.toString());
+        }
+    }
+    public void equal(View view) {
+
     }
 }
